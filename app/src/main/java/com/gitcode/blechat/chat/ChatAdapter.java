90b7c7b056872b56180de25b1bf67c7bba8b1bc4
zhangwenshuan <androizhang@gmail.com>
2019-06-10 09:50:00 +0800

    蓝牙聊天

---
+++ app/src/main/java/com/gitcode/blechat/chat/ChatAdapter.java
@@ -0,0 +1,88 @@
+package com.gitcode.blechat.chat;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.gitcode.blechat.R;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter {
+
+    private static final String TAG = "ChatAdapter";
+
+    public static final int SELF_CHAT = 0;
+    public static final int FRIEND_CHAT = 1;
+
+
+    private Context context;
+
+    private List<Chat> list;
+
+    public ChatAdapter(Context context, List<Chat> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+        if (viewType==SELF_CHAT) {
+            return new RightHolder(LayoutInflater.from(context).inflate(R.layout.adapter_chat_right, viewGroup, false));
+        } else {
+            return new LeftHolder(LayoutInflater.from(context).inflate(R.layout.adapter_chat_left, viewGroup, false));
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        if (viewHolder instanceof RightHolder) {
+            ((RightHolder) viewHolder).tvText.setText(list.get(i).getText());
+        } else {
+            ((LeftHolder) viewHolder).tvText.setText(list.get(i).getText());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (list.get(position).isSelf()) {
+            return SELF_CHAT;
+        } else {
+            return FRIEND_CHAT;
+        }
+    }
+
+    class LeftHolder extends RecyclerView.ViewHolder {
+
+        TextView tvText;
+
+        public LeftHolder(@NonNull View itemView) {
+            super(itemView);
+            tvText = itemView.findViewById(R.id.tvLeftChatText);
+        }
+
+    }
+
+
+    class RightHolder extends RecyclerView.ViewHolder {
+
+        TextView tvText;
+
+        public RightHolder(@NonNull View itemView) {
+            super(itemView);
+            tvText = itemView.findViewById(R.id.tvRightChatText);
+        }
+    }
+
+}
